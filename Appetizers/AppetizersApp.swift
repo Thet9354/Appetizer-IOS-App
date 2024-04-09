edf8851a023cc0882890deee66255531c961a6df
PhoonThetPine <thetpine254@gmail.com>
2024-04-09 10:20:49 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Phoon Thet Pine on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
