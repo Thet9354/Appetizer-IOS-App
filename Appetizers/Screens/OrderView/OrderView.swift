27cf70edc5f86f97ad038699573c7511ecdc023c
PhoonThetPine <thetpine254@gmail.com>
2024-04-18 22:45:36 +0800

    Appetizer IOS App - V5

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -9,29 +9,29 @@ import SwiftUI
 
 struct OrderView: View {
     
-    @State private var orderItems = MockData.orderItems
+    @EnvironmentObject var order: Order
     
     var body: some View {
         NavigationView {
             ZStack {
                 VStack {
                     List {
-                        ForEach(orderItems) { appetizer in
+                        ForEach(order.items) { appetizer in
                             AppetizerListCell(appetizer: appetizer)
                         }
-                        .onDelete(perform: deleteItems)
+                        .onDelete(perform: order.deleteItems)
                     }
                     .listStyle(PlainListStyle())
                     
                     Button {
                         print("order Placed")
                     } label: {
-                        APButton(title: "$99.99 - Place Order")
+                        APButton(title: "$\(order.totalPrice, specifier: "%.2f") - Place Order")
                     }
                     .padding(.bottom, 25)
                 }
                 
-                if orderItems.isEmpty {
+                if order.items.isEmpty {
                     EmptyStates(imageName: "empty-order", message: "You have no items in your order.\nPlease add an appetizer!")
                 }
             }
@@ -39,9 +39,7 @@ struct OrderView: View {
         }
     }
     
-    func deleteItems(at offesets: IndexSet) {
-        orderItems.remove(atOffsets: offesets)
-    }
+    
 }
 
 #Preview {
